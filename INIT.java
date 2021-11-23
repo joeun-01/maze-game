3b1aba0a6c88e9a50a179c24590e01b78102333a
정조은 <83575928+joeun-01@users.noreply.github.com>
2021-11-23 18:13:51 +0900

    Update INIT.java

--- INIT.java
+++ INIT.java
@@ -1,20 +1,23 @@
-package algorithms_maze;
+package maze;
 
 public class INIT {
-	static int MAZE_BOARD_HEIGHT = 10;
-	static int MAZE_BOARD_WIDTH = 6;
 	static int xpos;
 	static int ypos;
-	static char maze[][] = {
-			{'1','1','1','1','1','1'},
-			{'e','0','0','0','0','1'},
-			{'1','0','1','1','1','1'},
-			{'1','0','0','0','1','1'},
-			{'1','0','1','0','0','1'},
-			{'1','0','0','1','0','1'},
-			{'1','1','1','0','0','1'},
-			{'1','0','1','0','1','1'},
-			{'1','0','0','0','0','x'},
-			{'1','1','1','1','1','1'},
-	};
+	
+	static maze_5x5 maze1 = new maze_5x5();
+	static int[][] maze11 = maze1.maze_info();
+	static int MAZE_BOARD_HEIGHT1 = 5;
+	static int MAZE_BOARD_WIDTH1 = 5;
+	
+	static maze_7x7 maze2 = new maze_7x7();
+	static int[][] maze22 = maze2.maze_info();
+	static int MAZE_BOARD_HEIGHT2 = 7;
+	static int MAZE_BOARD_WIDTH2 = 7;
+	
+	static maze_10x10 maze3 = new maze_10x10();
+	static int[][] maze33 = maze3.maze_info();
+	static int MAZE_BOARD_HEIGHT3 = 10;
+	static int MAZE_BOARD_WIDTH3 = 10;
+	
+	
 }

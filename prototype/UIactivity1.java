dc4eb1c675b722c3a69419c9123ad22fd77283c8
kimgihean <83570546+kimgihean@users.noreply.github.com>
2021-11-24 13:50:33 +0900

    Update UIactivity1.java

--- prototype/UIactivity1.java
+++ prototype/UIactivity1.java
@@ -8,13 +8,26 @@ import java.awt.event.*;
 
 @SuppressWarnings("serial")
 public class UIactivity1 extends JFrame {
-	public UIactivity1() {
+	public UIactivity1(JButton button) {
+		String check = button.getText();
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UI1 frame = new UI1();
-					frame.setVisible(true);
-					
+					switch(check) {
+						case "Level 1" :
+							UI1 frame1 = new UI1();
+							frame1.setVisible(true);
+							break;
+						case "Level 2" :
+							UI1 frame2 = new UI1();
+							frame2.setVisible(true);
+							break;
+						case "Level 3" :
+							UI1 frame3 = new UI1();
+							frame3.setVisible(true);
+							break;
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}

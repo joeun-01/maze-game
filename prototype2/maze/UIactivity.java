c6ebd573098d0f4a065077bded8d96e952b492a3
정조은 <83575928+joeun-01@users.noreply.github.com>
2021-11-29 13:27:46 +0900

    Update UIactivity.java

--- prototype2/maze/UIactivity.java
+++ prototype2/maze/UIactivity.java
@@ -1,10 +1,7 @@
 package maze;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class UIactivity extends JFrame {
 	
@@ -13,18 +10,18 @@ public class UIactivity extends JFrame {
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				
+				// choose level 1 ~ 3 
 				try {
 					switch(check) {
-					case "Level 1":
+					case "Level 1":  // level 1
 						UI1 frame1 = new UI1();
 						frame1.setVisible(true);
 						break;
-					case "Level 2":
+					case "Level 2":  // level 2
 						UI2 frame2 = new UI2();
 						frame2.setVisible(true);
 						break;
-					case "Level 3":
+					case "Level 3":  // level 3
 						UI3 frame3 = new UI3();
 						frame3.setVisible(true);
 						break;
